60491cf22ff5119d6670b2f2b147c85e7e16762f
nayemDevs <nayemDevs@gmail.com>
2015-02-04 12:40:51 +0600

    post.php
    Added new file to post

---
+++ post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form posting</title>
+</head>
+<body>
+<div class="form">
+Hey <?php echo $_POST["name"];?>! Whats up?<br>
+Your e-mail address is <?php echo $_POST["email"];?><br>
+Your mother name is <?php echo $_POST["mname"];?><br>
+Your father name is <?php echo $_POST["fname"]; ?>
+</div>
+</body>
+</html>
